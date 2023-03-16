25b8dee4a708defb5141eeadfba50004c484f103
Renan Lucena <reenandeveloper@hotmail.com>
2023-03-16 20:33:53 -0300

    auto login created

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const UserModel = require('../models/User');
 
-router.get('/users', (req, res) => {
-  return res.json({ logged: true });
+router.get('/users', async (req, res) => {
+  const user = await UserModel.findOne({ _id: req.userLogged.id });
+  const token = req.headers.authorization.split(' ')[1];
+
+  return res.json({ user, token });
 });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use('/auth', AuthController);
 
 app.use('/admin', authenticateMiddleware, AdminController);
 
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 3100;
 
 app.listen(port, () => {
   console.log('o servidor está funcionando');
